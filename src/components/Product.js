ac3a106e38079223b100aa9a3fc9d0a68da597bb
Amdrii <andreyk557@gmail.com>
2023-03-27 22:51:09 +0300

    feat[product-btns]

--- src/components/Product.js
+++ src/components/Product.js
@@ -18,12 +18,18 @@ const Product = ({ product }) => {
             ></img>
           </div>
         </div>
-        <div className="absolute top-0 right-0 bg-red-500">
+        <div className="absolute top-6 -right-11 group-hover:right-5 bg-red-500/40 p-2 flex flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
           <button>
-            <div className="flex justify-center items-center text-white w-12 h-12">
+            <div className="flex justify-center items-center text-white w-12 h-12 bg-red-500">
               <BsPlus className="text-3xl" />
             </div>
           </button>
+          <Link
+            to={"/"}
+            className="w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl"
+          >
+            <BsEyeFill />
+          </Link>
         </div>
       </div>
       <div>2</div>
