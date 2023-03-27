eb4ac890bf979dafe627eb0f6c73c4cf76232bb9
Amdrii <andreyk557@gmail.com>
2023-03-28 00:02:54 +0300

    feat[sidebae/btn/close-open]

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,10 +7,22 @@ import CartItem from "../components/CartItem";
 import { SidebarContext } from "../contexts/SidebarContext";
 
 const Sidebar = () => {
-  const { isOpen, handelClose } = useContext(SidebarContext);
+  const { isOpen, handleClose } = useContext(SidebarContext);
   return (
-    <div className="w-full bg-white fixed top-0 h-full shadow-2xl md:-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-5 lg:px-[35px]">
-      Sidebar
+    <div
+      className={` ${
+        isOpen ? "right-0" : "-right-full"
+      } w-full bg-white fixed top-0 h-full shadow-2xl md:-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-5 lg:px-[35px]`}
+    >
+      <div className="flex items-center justify-between py-6 border-b">
+        <div className="uppercase text-sm font-semibold"> Shopping Bag (0)</div>
+        <div
+          onClick={handleClose}
+          className="cursor-pointer w-8 h-8 flex justify-center items-center"
+        >
+          <IoMdArrowForward className="text-2xl" />
+        </div>
+      </div>
     </div>
   );
 };
